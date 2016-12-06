8c756673f7d7bc61323c2038ffe0586f87eebeea
Robin Hoogervorst <robin9975@gmail.com>
2016-12-06 15:54:17 +0100

    workspace configuration for desktop

--- custom/lock.sh
+++ custom/lock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ICON=$HOME/.dotfiles/custom/lock_icon.png
-TMPBG=/tmp/screen_for_i3_lock.png
-scrot /tmp/screen_for_i3_lock.png
+TMPBG=/tmp/screen_for_i3_lock.jpg
+scrot /tmp/screen_for_i3_lock.jpg
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG -draw "image over 896,476 0,0 '$ICON'" -matte $TMPBG
-convert $TMPBG -draw "image over 2816,476 0,0 '$ICON'" -matte $TMPBG
-i3lock -i $TMPBG
+convert $TMPBG -draw "image over 2816,476 0,0 '$ICON'" -matte /tmp/screen_for_i3_lock.png
+i3lock -i /tmp/screen_for_i3_lock.png 
