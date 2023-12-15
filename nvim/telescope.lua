04eedfbf9e54c56f5e2ca54dc48b96593ecee6bc
Robin Hoogervorst <robin@datogether.nl>
2023-12-15 12:03:54 +0100

    nvim improvements

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -1,7 +1,14 @@
 
 require('telescope').setup()
 
+local coc = require('telescope').load_extension('coc')
+
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+
+local opts = {}
+opts['get_all'] = true
+vim.keymap.set('n', '<leader>d', function() return coc.diagnostics(opts) end, {} )
